341c0b12905f7c48c96ecfd639b29c280bd76677
rocrill <crillyr@tcd.ie>
2021-10-11 21:12:12 +0000

    Added javascript to generate dish names with images. Added objects to arrays.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,38 +4,85 @@ function generateMeal() {
     document.getElementById("carb-portion").style.backgroundImage = "url('./assets/images/rice.png')";
     
     let carb = [
-        "url('./assets/images/fried-rice.jpg')",
-        "url('./assets/images/mashed-potatoes.jpg')",
-        "url('./assets/images/2-slices-sourdough.jpg')",
-        "url('./assets/images/portion-of-penne.jpg')",
-        "url('./assets/images/sweet-potato-fries.jpg')",
+        {
+            name:"Fried rice",
+            image:"url('./assets/images/fried-rice.jpg')",
+        },
+        {
+            name:"Mashed potatoes",
+            image:"url('./assets/images/mashed-potatoes.jpg')",
+        },
+        {
+            name:"2 slices of sourdough bread",
+            image:"url('./assets/images/2-slices-sourdough.jpg')",
+        },
+        {
+            name:"Portion of penne pasta with pesto",
+            image:"url('./assets/images/portion-of-penne.jpg')",
+        },
+        {
+            name:"Sweet potato fries",
+            image:"url('./assets/images/sweet-potato-fries.jpg')",
+        },
+        
     ];
 
     let carbSelection = getRandomInt(0, carb.length)
     
     let veg = [
-        "url('./assets/images/roast-asparagus.jpg')",
-        "url('./assets/images/roasted-peppers.jpg')",
-        "url('./assets/images/spinach-salad.jpg')",
-        "url('./assets/images/broccoli.jpg')",
+        {
+            name:"Roasted asparagus",
+            image:"url('./assets/images/roast-asparagus.jpg')",
+        },
+        {
+            name:"Roasted peppers",
+            image:"url('./assets/images/roasted-peppers.jpg')",
+        },
+        {
+            name:"Spinach salad",
+            image:"url('./assets/images/spinach-salad.jpg')",
+        },
+        {
+            name:"Steamed broccoli",
+            image:"url('./assets/images/broccoli.jpg')",
+        },
         
     ];
 
     let vegSelection = getRandomInt(0, veg.length)
 
     let protein = [
-        "url('./assets/images/pan-fried-salmon.jpg')",
-        "url('./assets/images/pan-seared-steak.jpg')",
-        "url('./assets/images/roast-cod.jpg')",
-        "url('./assets/images/roast-chicken.jpg')",
-    ]
+        {
+            name:"Pan-fried salmon",
+            image:"url('./assets/images/pan-fried-salmon.jpg')",
+        },
+        {
+            name:"Pan-seared steak",
+            image:"url('./assets/images/pan-seared-steak.jpg')",
+        },
+        {
+            name:"Roast cod",
+            image:"url('./assets/images/roast-cod.jpg')",
+        },
+        {
+            name:"Roast chicken",
+            image:"url('./assets/images/roast-chicken.jpg')",
+        },
+        
+    ];
 
-    let proteinSelection = getRandomInt(0, veg.length)
+    let proteinSelection = getRandomInt(0, protein.length)
 
-    document.getElementById("carb-portion").style.backgroundImage = carb[carbSelection];
-    document.getElementById("veg-portion").style.backgroundImage = veg[vegSelection];
-    document.getElementById("protein-portion").style.backgroundImage = protein[proteinSelection];
+    document.getElementById("carb-portion").style.backgroundImage = carb[carbSelection].image;
+    document.getElementById("veg-portion").style.backgroundImage = veg[vegSelection].image;
+    document.getElementById("protein-portion").style.backgroundImage = protein[proteinSelection].image;
+
+    document.getElementById("carb-result").innerHTML = carb[carbSelection].name;
+    document.getElementById("protein-result").innerHTML = protein[proteinSelection].name;
+    document.getElementById("veg-result").innerHTML = veg[vegSelection].name;
+
 }
+
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
