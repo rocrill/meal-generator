40e0639efa1beb6d32eea7e8fdffc537653e4351
rocrill <crillyr@tcd.ie>
2021-10-11 19:42:08 +0000

    Add images and veg and protein sections of generate meal function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,14 +5,34 @@ function generateMeal() {
     
     let carb = [
         "url('./assets/images/rice.png')",
-        "url('./assets/images/roastbroc.png')",
+        "url('./assets/images/mashed-potatoes.jpg')",
         "url('./assets/images/logo.png')"
     ];
 
     let carbSelection = getRandomInt(0, carb.length)
     
+    let veg = [
+        "url('./assets/images/roastbroc.png')",
+        "url('./assets/images/roast-asparagus.jpg')",
+        "url('./assets/images/roasted-peppers.jpg')",
+        "url('./assets/images/spinach-salad.jpg')",
+        "url('./assets/images/fried-carrots-and-broccoli.jpg')",
+    ];
+
+    let vegSelection = getRandomInt(0, veg.length)
+
+    let protein = [
+        "url('./assets/images/pan-fried-salmon.jpg')",
+        "url('./assets/images/pan-seared-steak.jpg')",
+        "url('./assets/images/roast-cod.jpg')",
+        "url('./assets/images/roast-chicken.jpg')",
+    ]
+
+    let proteinSelection = getRandomInt(0, veg.length)
+
     document.getElementById("carb-portion").style.backgroundImage = carb[carbSelection];
-    /*document.getElementById("veg-portion").style.backgroundImage = carb[carbSelection];*/
+    document.getElementById("veg-portion").style.backgroundImage = veg[vegSelection];
+    document.getElementById("protein-portion").style.backgroundImage = protein[proteinSelection];
 }
 
 function getRandomInt(min, max) {
