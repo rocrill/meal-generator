1e592d326ee6b87a4361e35fc3d366926c94dc53
rocrill <crillyr@tcd.ie>
2021-10-11 19:17:42 +0000

    Build out 'Generate Meal' function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,20 @@ function generateMeal() {
     document.getElementById("veg-portion").style.backgroundImage = "url('./assets/images/roastbroc.png')";
     document.getElementById("carb-portion").style.backgroundImage = "url('./assets/images/rice.png')";
     
+    let carb = [
+        "url('./assets/images/rice.png')",
+        "url('./assets/images/roastbroc.png')",
+        "url('./assets/images/logo.png')"
+    ];
 
+    let carbSelection = getRandomInt(0, carb.length)
+    
+    document.getElementById("carb-portion").style.backgroundImage = carb[carbSelection];
+    /*document.getElementById("veg-portion").style.backgroundImage = carb[carbSelection];*/
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); 
 }
