a5fad279a496bda7fa0681cda61412bc726b5cac
rocrill <crillyr@tcd.ie>
2021-10-16 16:25:46 +0000

    Start building out nutrition quiz

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,4 +1,4 @@
-
+/*
 
 const myQuestions = [
     {
@@ -24,3 +24,19 @@ const myQuestions = [
   ];
 
   const submitButton = document.getElementById('submit');
+*/
+
+
+  function calculateScore() {
+    let score = 0;
+
+    if (document.getElementById("q1b").checked) {
+      score = score+1;
+    }
+    if (document.getElementById("q2a").checked) {
+      score = score+1;
+    }
+  
+
+  document.getElementById("quiz-result").innerHTML = "Result:"+ (score/2)*100 + "%";
+  }
