5ccc9142fe492609bb327e8257481267ce65944e
rocrill <crillyr@tcd.ie>
2021-10-16 13:59:14 +0000

    Start questions for quiz. Add basic quiz structure to quiz.js file.

---
+++ assets/js/quiz.js
@@ -0,0 +1,26 @@
+
+
+const myQuestions = [
+    {
+      question: "Which one of the following would be considered a protein?",
+      answers: {
+        a: "Potato",
+        b: "fish",
+        c: "apple",
+        d: "Broccoli"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "What is the recommended serving size of carb per meal?",
+      answers: {
+        a: "a fist size",
+        b: "a finger size",
+        c: "1/2 of a standard dinner plate"
+        d: "3/4 of a standard dinner plate"
+      },
+      correctAnswer: "a"
+    }
+  ];
+
+  const submitButton = document.getElementById('submit');
