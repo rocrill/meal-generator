569ba49db11c8c82ac4ff91f642f660b5eef53ff
rocrill <crillyr@tcd.ie>
2021-10-19 09:35:06 +0000

    Tidy code in quiz.js file

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,57 +1,46 @@
+function calculateScore() {
+  let score = 0;
 
-  function calculateScore() {
-    let score = 0;
-
-    if (document.getElementById("q1b").checked) {
-      score = score+1;
-    }
-    if (document.getElementById("q2a").checked) {
-      score = score+1;
-    }
-    if (document.getElementById("q3c").checked) {
-      score = score+1;
-    }
-    if (document.getElementById("q4b").checked) {
-      score = score+1;
-    }
-    if (document.getElementById("q5d").checked) {
-      score = score+1;
-    }
-    if (document.getElementById("q6b").checked) {
-      score = score+1;
-    }
-
+  if (document.getElementById("q1b").checked) {
+    score = score+1;
+  }
+  if (document.getElementById("q2a").checked) {
+    score = score+1;
+  }
+  if (document.getElementById("q3c").checked) {
+    score = score+1;
+  }
+  if (document.getElementById("q4b").checked) {
+    score = score+1;
+  }
+  if (document.getElementById("q5d").checked) {
+    score = score+1;
+  }
+  if (document.getElementById("q6b").checked) {
+    score = score+1;
+  }
+ 
   let scorePercent = Math.round((score/6)*100);
-
   document.getElementById("quiz-result").innerHTML = "Result:"+ scorePercent + "%";
   
- if (scorePercent<30) {
-   document.getElementById("result-message").innerHTML = `Wow, you got ${scorePercent}%! ... you could probably use our meal generator!`;
- }
-
- if (scorePercent>=30) {
-  document.getElementById("result-message").innerHTML = `Hmm, you got ${scorePercent}%! ... your nutritional knowledge could be worse, but you should still probably give our meal generator a go!`;
-}
-
-if (scorePercent>=60) {
-  document.getElementById("result-message").innerHTML = `Nice! You got ${scorePercent}%! :)`;
-}
-
-if (scorePercent>=90) {
-  document.getElementById("result-message").innerHTML = `Huzzah! You got ${scorePercent}%! You must be a qualified nutritionist!`;
-}
-
+  if (scorePercent>=90) {
+    document.getElementById("result-message").innerHTML = `Huzzah! You got ${scorePercent}%! You must be a qualified nutritionist!`;
+  } else if (scorePercent>=60) {
+    document.getElementById("result-message").innerHTML = `Nice! You got ${scorePercent}%! :)`;
+  } else if (scorePercent>=30) {
+    document.getElementById("result-message").innerHTML = `Hmm, you got ${scorePercent}%! ... your nutritional knowledge could be worse, but you should still probably give our meal generator a go!`;
+  } else {
+    document.getElementById("result-message").innerHTML = `Wow, you got ${scorePercent}%! ... you could probably use our meal generator!`;  
+  }
 }
 
 function refreshPage() {
-  
   const radio = document.getElementsByTagName("input");
 
   for(let i=0; i < radio.length; i++) {
-      radio[i].checked = false;
+    radio[i].checked = false;
   }
-
   document.getElementById("result-message").innerHTML = "";
   document.getElementById("quiz-result").innerHTML = "";
- 
+  
 }
