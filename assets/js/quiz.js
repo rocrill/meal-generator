e07a8ca4bc476b6c2f420017bb9f0fcef21558d0
rocrill <crillyr@tcd.ie>
2021-10-19 13:49:53 +0000

    Add images to quiz result message function. Update result image and message styling

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -29,12 +29,21 @@ function calculateScore() {
   /* Score determines result message */
   if (scorePercent>=90) {
     document.getElementById("result-message").innerHTML = `Congrats ${fName}, you got ${scorePercent}%! You must be a qualified nutritionist!`;
+    document.getElementById("result-image").style.backgroundImage = "url('./assets/images/nutritionist.jpg')";
+    document.getElementById("result-and-message-container").style.display= "block";
   } else if (scorePercent>=60) {
     document.getElementById("result-message").innerHTML = `Nice work ${fName}! You got ${scorePercent}%! :)`;
+    document.getElementById("result-image").style.backgroundImage = "url('./assets/images/nice-work.jpg')";
+    document.getElementById("result-and-message-container").style.display= "block";
   } else if (scorePercent>=30) {
     document.getElementById("result-message").innerHTML = `Hmm, you got ${scorePercent}% ${fName}! ... your nutritional knowledge could be worse, but you should still probably give our meal generator a go!`;
+    document.getElementById("result-image").style.backgroundImage = "url('./assets/images/just-ok.jpg')";
+    document.getElementById("result-and-message-container").style.display= "block";
   } else {
-    document.getElementById("result-message").innerHTML = `Wow ${fName}, you got ${scorePercent}%! ... you could probably use our meal generator!`;  
+    document.getElementById("result-message").innerHTML = `Wow ${fName}, you got ${scorePercent}%! ... you could probably use our meal generator!`;
+    document.getElementById("result-image").style.backgroundImage = "url('./assets/images/bad-score.jpg')";
+    document.getElementById("result-image").style.backgroundColor = 'beige';
+    document.getElementById("result-and-message-container").style.display= "block";  
   }
 }
 
@@ -46,5 +55,6 @@ function refreshPage() {
   }
   document.getElementById("result-message").innerHTML = "";
   document.getElementById("quiz-result").innerHTML = "";
+  document.getElementById("result-and-message-container").style.display= "none";
   
 }
